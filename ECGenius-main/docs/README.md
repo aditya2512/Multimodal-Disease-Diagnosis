4c59896c3332de404837f50c4f387c73375d07be
Aditya <32999227+aditya2512@users.noreply.github.com>
2024-12-19 17:21:33 -0500

    Delete ECGenius-main directory

--- ECGenius-main/docs/README.md
+++
@@ -1,4 +0,0 @@
-# To run docker container:
-
-- docker-compose up --build
-- Go to http://localhost:4000 to see your live site!
--- ECGenius-main/models/rvenet/all_models.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-
-from models.rvenet.CardiacCycleRNN import CardiacCycleRNN
-from models.rvenet.ResNetLSTM import ResNetLSTM
-from models.rvenet.ResNextTemporal import ResNextTemporal
-from models.rvenet.CardiacCycleTransformer import CardiacCycleTransformer
-
-model_union = Union[CardiacCycleRNN, ResNetLSTM, ResNextTemporal,CardiacCycleTransformer]
